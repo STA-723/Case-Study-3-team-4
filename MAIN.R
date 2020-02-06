335da90a38ab3a6dd03a5a39a7c032d4cf8250e8
Evan <evanknoxgrad@gmail.com>
2020-02-06 12:20:58 -0500

    Unzipped the files, had to stop there for now.  Hopefully this isn't like 55 files all individually listed in the repo.

---
+++ MAIN.R
@@ -0,0 +1,12 @@
+##MAIN
+##Evan Knox, Joe Mathews, and Alessandro Zito
+
+#Unziping data files
+#unzip("Harvard_CAS_1993.zip")
+#unzip("Harvard_CAS_1997.zip")
+#unzip("Harvard_CAS_1999.zip")
+#unzip("Harvard_CAS_2001.zip")
+
+#data1=read.table("Harvard_CAS_1993/DS0001/06577-0001-Data.txt")
+#OK, so the data have to be read in via the record layout with some annoying,
+#albeit standard, column-based stuff.  Leaving for now.  
