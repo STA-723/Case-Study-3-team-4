5023e4a806dd49663e79ed3b351378c58fbe781f
Evan <evanknoxgrad@gmail.com>
2020-02-07 13:42:22 -0500

    Reading formatting out of txt files, loading 1993 dataset based on that.

--- MAIN.R
+++ MAIN.R
@@ -7,6 +7,27 @@
 #unzip("Harvard_CAS_1999.zip")
 #unzip("Harvard_CAS_2001.zip")
 
-#data1=read.table("Harvard_CAS_1993/DS0001/06577-0001-Data.txt")
-#OK, so the data have to be read in via the record layout with some annoying,
-#albeit standard, column-based stuff.  Leaving for now.  
+#Reading in column names and widths from record layout files
+datalabels93=read.table("Harvard_CAS_1993/DS0001/06577-0001-Record_layout.txt",skip=10)
+datalabels93$width=(datalabels93$V3-datalabels93$V2)+1
+
+#97 still isn't working for some reason -- mentions line 427, but
+#no apparent problem on that line of the file.  Try nrows=400 or something
+#and look for errors in formatting, I guess.
+datalabels97=read.table("Harvard_CAS_1997/DS0001/03163-0001-Record_layout.txt",skip=10)
+datalabels97$width=(datalabels97$V3-datalabels97$V2)+1
+
+datalabels99=read.table("Harvard_CAS_1999/DS0001/03818-0001-Record_layout.txt",skip=10)
+datalabels99$width=(datalabels99$V3-datalabels99$V2)+1
+
+datalabels01=read.table("Harvard_CAS_2001/DS0001/04291-0001-Record_layout.txt",skip=9)
+datalabels01$width=(datalabels01$V3-datalabels01$V2)+1
+
+#Loading data (just one takes forever, apparently.  Consider making these into
+#files and commiting/pushing them.)
+data93=read.fwf("Harvard_CAS_1993/DS0001/06577-0001-Data.txt",widths=datalabels93$width,col.names = datalabels93$V1)
+data97
+data99
+data01
+
+head(data93$STUDY_ID)
