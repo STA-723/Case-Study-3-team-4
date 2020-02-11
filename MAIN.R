61345cd9e21ae4b88082c50798c11a9200114895
Evan <evanknoxgrad@gmail.com>
2020-02-11 08:40:11 -0500

    Fixd 97 data, wrote file

--- MAIN.R
+++ MAIN.R
@@ -14,8 +14,10 @@ datalabels93$width=(datalabels93$V3-datalabels93$V2)+1
 #97 still isn't working for some reason -- mentions line 427, but
 #no apparent problem on that line of the file.  Try nrows=400 or something
 #and look for errors in formatting, I guess.
-datalabels97=read.table("Harvard_CAS_1997/DS0001/03163-0001-Record_layout.txt",skip=10)
+datalabels97=read.table("Harvard_CAS_1997/DS0001/03163-0001-Record_layout.txt",skip=10,nrows=426)
 datalabels97$width=(datalabels97$V3-datalabels97$V2)+1
+widths97=c(datalabels97$width,11,11,3,4)
+labels97=c(datalabels97$V1,"DATERECD","DATESCAN","FORMCODE","IPEDS")
 
 datalabels99=read.table("Harvard_CAS_1999/DS0001/03818-0001-Record_layout.txt",skip=10)
 datalabels99$width=(datalabels99$V3-datalabels99$V2)+1
@@ -25,12 +27,12 @@ datalabels01$width=(datalabels01$V3-datalabels01$V2)+1
 
 #Loading data (just one takes forever, apparently.  Consider making these into
 #files and commiting/pushing them.)
-data93=read.fwf("Harvard_CAS_1993/DS0001/06577-0001-Data.txt",widths=datalabels93$width,col.names = datalabels93$V1)
-data97
-data99
-data01
+data97=read.fwf("Harvard_CAS_1997/DS0001/03163-0001-Data.txt",widths=widths97,col.names=labels97)
+?write.csv
+write.csv(data97,file="Data/data97.csv")
 
 #I think it works!  At least for this one.
-head(data93$STUDY_ID)
-
+data93=read.csv("Data/data93.csv")
+data99=read.csv("Data/data99.csv")
+data01=read.csv("Data/data01.csv")
 
