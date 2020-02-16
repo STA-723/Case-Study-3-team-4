9988cc19148259bdefc8931586a85c3c595005b8
josephmathews <jrm133@duke.edu>
2020-02-15 19:01:33 -0500

    model

--- gruped_LASSO.R
+++ gruped_LASSO.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(gglasso)
-df <- read.csv("/Users/antonio/Dropbox/Duke/Courses_Spring2020/Cases_Studies/Case_3/Case_3/Data/final_data.csv")
+library(grplasso)
+df <- read.csv("/Users/josephmathews/Desktop/Case Studies/CaseStudy3/Case-Study-3-team-422/Data/final_data.csv")
 f1 <- function(var) {
   ifelse((var == 3  | var == 5 | var == 6 | var == 7),1,0)
 }
@@ -31,11 +32,11 @@ df <- df %>% mutate_at(vars(AlchUse_father,AlchUse_mother, alchol_use),f1)
 
 
 df <- df %>% mutate_at(vars(X30_days_alchol,X30_days_drink_usually,X30_days_drunk,AlchUse_famfeel,
-                            "binge_4.","binge_5.",starts_with("DP"),Educ_satisfaction,
+                            "binge_4.","binge_5.",Educ_satisfaction,
                             HS_drinks_5.,HS_n_drinks,HS_times_drank),as.ordered)
 df <- df %>% mutate_at(vars(AA_meeting,AlchUse_father,AlchUse_mother,asian,father_schooling,
                             gender,greek_member,starts_with("live_"),Marital_status,mother_schooling,
-                            native_american,other,religion,Spanish,white,year,year_in_school,alchol_use),as.factor) %>%
+                            native_american,other,religion,Spanish,white,year,year_in_school,alchol_use,starts_with("DP")),as.factor) %>%
   rename(other_race = other ,age_individ= age)
 
 X <- df %>% dplyr::select(-c("HS_drinks_5","last_drink",starts_with("DAYE1"),"D","survey_weight")) 
@@ -60,6 +61,28 @@ for(n in 1:length(varnames)){
     }
   }
 }
-
 da <- list("X"=M,"y"=y)
-model_lasso <- gglasso(x=da$X,y=da$y,group=groups_contrast,loss="logit")
+model_lasso <- gglasso(x=da$X,y=da$y,group=groups_contrast,nlambda=1,lambda = 0.008,loss="logit")
+keep <- unique(c(which(grepl("religion",model_lasso$beta %>% rownames() )),
+                 which(model_lasso$beta != 0),
+                 which(grepl("Spanish",model_lasso$beta %>% rownames())),
+                 which(grepl("asian",model_lasso$beta %>% rownames() )),
+                 which(grepl("white",model_lasso$beta %>% rownames() )),
+                 which(grepl("black",model_lasso$beta %>% rownames() ))))
+
+reduced_df <- data.frame(y,M[,keep]) 
+reduced_df$D <- ifelse(reduced_df$D==-1,0,1)
+
+glm(D ~., data=reduced_df,family=binomial) %>% summary()
+
+
+
+
+model_lasso2 <- cv.gglasso(x=da$X,y=da$y,group=groups_contrast,lambda = seq(0.001,0.01,length.out = 10),pred.loss = c("loss"))
+data <- data.frame(y,M)
+
+
+y[,1]
+model_lasso <- grplasso(M,y[,1],index=groups_contrast,lambda=seq(1,0.001,length.out = 25))
+
+
